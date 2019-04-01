a1b87b1f8a345e7bd2dc8250e3197d2a859ec5a0
Simon Böhm <6198@htl.rennweg.at>
2019-04-01 14:16:18 +0200

    REAL Initial commit

---
+++ NetWeaverServer/Main/CollectionUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetWeaverServer.Main
+{
+
+    public static class CollectionUtils
+    {
+        public static string ToFormat<T>(this ICollection<T> collection)
+        {
+            string type = collection.GetType().Name;
+            if (collection is List<int> || collection is ISet<int>)
+                return type.Substring(0, type.Length - 2)
+                    + " [" + string.Join(", ", collection) + "]";
+            else
+                return type.Substring(0, type.Length - 2)
+                    + " [\"" + string.Join("\", \"", collection) + "\"]";
+        }
+
+        public static string ToFormat<T>(this T[] array)
+        {
+            if (array is int[])
+                return "[" + string.Join(", ", array) + "]";
+            else
+                return "[\"" + string.Join("\", \"", array) + "\"]";
+        }
+    }
+}
---
+++ NetWeaverServer/Main/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using NetWeaverServer.GraphicalUI;
+using NetWeaverServer.Jobs;
+
+namespace NetWeaverServer.Main
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            POCServer();
+        }
+
+        public static void POCLogging()
+        {
+            LoggingOperation eventlog = new LoggingOperation("MCONServer");
+            Console.WriteLine(eventlog.GetType());
+
+            eventlog.Info(eventlog, "Program has been started");
+            Console.ReadKey();
+            EventLog.Delete("MCON");
+        }
+
+        public static void POCServer()
+        {
+            GUIServerInterface guiServerInterface = new GUIServerInterface();
+            GUI gui = new GUI(guiServerInterface);
+            Server server = new Server(guiServerInterface);
+            Console.WriteLine("Start of Server");
+        }
+    }
+}
